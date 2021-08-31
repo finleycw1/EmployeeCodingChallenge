1d69ce4f9236fd91df08f1c88fc7be0f02b697ec
Chris Finley <finleycw1@gmail.com>
2021-08-30 23:06:42 -0400

    Remove obsolete route file.

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Employees', employees: req.app.locals.employees});
-});
-
-module.exports = router;
